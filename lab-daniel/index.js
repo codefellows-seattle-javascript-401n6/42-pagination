c7d7b47f3d463a85d6922305d9f33e6321ecdd3b
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-06-05 19:42:57 -0700

    app.js renduring on page. added csv file.

--- lab-daniel/index.js
+++ lab-daniel/index.js
@@ -4,12 +4,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI)
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 app.get('/', (req, res) => {
     res.send({data: [46, 2]});
 })
+
+const Bundler = require('parcel-bundler');
+const bundler = new Bundler('./public/index.html');
+
+app.use(bundler.middleware());
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 })
---
+++ lab-daniel/public/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>NBA Player Salaries</h1>
+                <h2>2017-2018</h2>
+            </React.Fragment>
+        )
+    }
+}
+
+let root = document.getElementById('root');
+ReactDOM.render(<App />, root);
